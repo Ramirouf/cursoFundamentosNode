deee18ba0553e664c39d1dd2f2bf08e994e1d5ed
Ramiro Uffelmann <ramirouf@gmail.com>
2022-08-11 11:49:30 -0300

    Finished 21th class (building modules)

---
+++ packages/module/es6/index.mjs
@@ -0,0 +1,4 @@
+import module1 from './module.mjs';
+
+console.log(module1.prop1);
+module1.greet();
---
+++ packages/module/index.js
@@ -0,0 +1,7 @@
+// Bring our module
+// Execute a function from it
+
+const module1 = require('./module');
+
+console.log(module1.prop1);
+module1.greet();
